966e6112fa626dbef15e5ff47eb83a3b58653456
i4dabber <beratkaya@live.dk>
2019-01-30 10:54:01 +0000

    Dette er en test

---
+++ Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Lommeregner
+    {
+        //forward declared functions - plus impl
+        public double Add(double a, double b)
+        {         
+            return a + b;
+   
+        }
+        public double Subtract(double a, double b)
+        {
+            
+            return a - b;
+        }
+        public double Multiply(double a, double b)
+        {
+        
+            return a * b;
+        }
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Lommeregner test1 = new Lommeregner();
+            test1.Add(2.2, 3.2);
+            
+
+            Lommeregner test2 = new Lommeregner();
+            test2.Subtract(5.5, 2.5);
+
+            Lommeregner test3 = new Lommeregner();
+            test2.Multiply(5.5, 2.5);
+
+            Lommeregner test4 = new Lommeregner();
+            test2.Power(2.1, 3.1);*/
+        }
+    }
+}
+
---
+++ ClassLibrary1/calculatorunitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Calculator;
+
+namespace Calculator.Test.Unit
+{
+    [TestFixture]
+    public class calculatorunitTest
+    {
+        [Test]
+        public void Add_Add2and4_Returns6()
+        {
+            var uut = new Lommeregner();
+            Assert.That(uut.Add(3.5, 2.5), Is.EqualTo(6));
+            
+         }
+
+        [Test]
+        public void Subract_Subtract10and4_Returns6()
+        {
+            var uut = new Lommeregner();
+            Assert.That(uut.Subtract(10.0, 4.0), Is.EqualTo(6));
+        }
+
+    }
+}
